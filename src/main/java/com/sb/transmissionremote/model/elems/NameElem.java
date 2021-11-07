45f784e316da43d8add4d4b5601f1fbd40f85fd4
Sergey Bochkov <bochkovsa@gmail.com>
2021-11-07 11:02:26 +0500

    set max name size

--- src/main/java/com/sb/transmissionremote/model/elems/NameElem.java
+++ src/main/java/com/sb/transmissionremote/model/elems/NameElem.java
@@ -8,12 +8,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public final class NameElem implements Element {
 
+    private static final int MAX_LENGTH = 60;
+
     private final String name;
 
     @Override
     public JComponent graphic() {
-        var nameLabel = new JLabel(name);
+        var nameLabel = new JLabel(sizedName());
         nameLabel.setFont(new Font("SansSerif", Font.BOLD, 12));
         return nameLabel;
+    }
+
+    public String sizedName() {
+        if (name.length() > MAX_LENGTH) {
+            if (name.substring(name.lastIndexOf('.')).length() == 4) {
+                return name.substring(0, MAX_LENGTH - 4) + "..." + name.substring(name.lastIndexOf('.') + 1);
+            }
+            return name.substring(0, MAX_LENGTH) + "...";
+        }
+        return name;
     }
 }
